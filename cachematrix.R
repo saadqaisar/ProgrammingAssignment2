f86dc054a57ee4e33635503b376bcea4b14d1665
saadqaisar <sbq_pk@yahoo.com>
2016-03-09 17:49:35 +0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,16 @@
 ## functions do
 
 ## Write a short comment describing this function
+##1.	set the value of the matrix
+##2.	get the value of the matrix
+##3.	set the value of the inverse
+##4.	get the value of the inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
+
+
+return(matrix)
 
 }
 
